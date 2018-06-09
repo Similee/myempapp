da6795b4dd6f201cc29db6ee43a3d21271f8a453
similee <simileoluwa_babs@yahoo.com>
2018-06-09 13:36:00 +0100

    First Commit

---
+++ empSalaryApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace empSalaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            
+            while (y < 3)
+            {
+                Console.WriteLine("Please type in hourly rate");
+                int hourlyrate = Convert.ToInt32(Console.ReadLine());
+              
+                   if (hourlyrate <= 40)
+                    {
+                        Console.WriteLine(hourlyrate * 10);
+                    }
+                    else
+                    {
+                        Console.WriteLine((400) + ((hourlyrate - 40) * 15));
+                    }
+
+             
+
+                y++;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
